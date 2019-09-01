9f1ab12596dd140403819e56c05813108d098584
xq <568163103@qq.com>
2019-09-02 01:31:05 +0800

    线程演示

--- lesson-xq/src/main/java/com/deep/in/java/thread/ThreadDemo.java
+++ lesson-xq/src/main/java/com/deep/in/java/thread/ThreadDemo.java
@@ -1,4 +1,8 @@
 package com.deep.in.java.thread;
 
 public class ThreadDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
 }
