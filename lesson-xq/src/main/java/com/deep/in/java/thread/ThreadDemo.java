7945ecde5b52d835e03019cc68e9962789d43aa7
xq123456 <568163103@qq.com>
2019-09-02 11:37:55 +0800

    sss

--- lesson-xq/src/main/java/com/deep/in/java/thread/ThreadDemo.java
+++ lesson-xq/src/main/java/com/deep/in/java/thread/ThreadDemo.java
@@ -2,7 +2,6 @@ package com.deep.in.java.thread;
 
 public class ThreadDemo {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+
     }
 }
