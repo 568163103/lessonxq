2a62de0d91c36d7d25cc18ce667a002ceeef6c52
xq <568163103@qq.com>
2019-09-08 20:47:40 +0800

    no message

--- spring-gateway/src/main/java/com/xq/GateWayApplication.java
+++ spring-gateway/src/main/java/com/xq/GateWayApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * {@link EnableDiscoveryClient }
+ * @author xq
+ * @descriptions 网关启动类
+ * @since 2019/9/8
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GateWayApplication {
--- spring-provider/src/main/java/com/xq/SpringCloudLidongProviderApplication.java
+++ spring-provider/src/main/java/com/xq/SpringCloudLidongProviderApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 消息提供者启动类
+ * @author xq
+ * @since 2019/9/8
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudLidongProviderApplication {
