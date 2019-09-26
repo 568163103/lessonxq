d2fca044d7dcff2037c14300323595810157320e
xq123456 <568163103@qq.com>
2019-09-26 17:02:22 +0800

    no message

---
+++ spring-zabbix/src/main/java/com/xq/controller/AlarmInfoController.java
@@ -0,0 +1,35 @@
+package com.xq.controller;
+
+import com.xq.domain.Events;
+import com.xq.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName
+ * @Description
+ * @Auror
+ * @Date
+ * @Version
+ **/
+
+@RestController
+@RequestMapping("/alarm")
+public class AlarmInfoController {
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping(value = "/alarmInfoList",produces = "application/json;charset=utf-8")
+    public Map<String,Object> alarmInfoList(){
+        Map<String,Object> result = new HashMap<>(16);
+        List<Events> eventsList = eventService.findAllEvents();
+        result.put("eventsList",eventsList);
+        return result;
+    }
+}
