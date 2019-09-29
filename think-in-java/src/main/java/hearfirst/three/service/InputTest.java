df300db16e1bc6fd16cfbcbc76210f5bf7ace2c3
xq <568163103@qq.com>
2019-09-29 13:44:50 +0800

    no message

--- think-in-java/src/main/java/hearfirst/three/service/InputTest.java
+++ think-in-java/src/main/java/hearfirst/three/service/InputTest.java
@@ -12,12 +12,12 @@ public class InputTest {
         byte [] b = new byte[3];
         b[0]='c';
         InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("D:\\test.txt")));
-         while ((in.read(b,1,2)) !=-1){
-             for (int i = 0; i <b.length ; i++) {
-                 System.out.println("c=="+(char) b[i]);
-             }
-         }
+        while ((in.read(b,1,2)) !=-1){
+            for (int i = 0; i <b.length ; i++) {
+                System.out.println("c=="+(char) b[i]);
+            }
+        }
 
-         in.close();
+        in.close();
     }
 }
