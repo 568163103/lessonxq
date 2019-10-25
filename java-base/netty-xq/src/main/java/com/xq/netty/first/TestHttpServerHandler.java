8efae5daf46025429e2a17d2c0ef2ac042bd56f3
xq123456 <568163103@qq.com>
2019-10-25 17:07:06 +0800

    no message

--- java-base/netty-xq/src/main/java/com/xq/netty/first/TestHttpServerHandler.java
+++ java-base/netty-xq/src/main/java/com/xq/netty/first/TestHttpServerHandler.java
@@ -1,11 +1,14 @@
 package com.xq.netty.first;
 
+import com.sun.jndi.toolkit.url.Uri;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
+
+import java.net.URI;
 
 /**
  * @ClassName
@@ -19,6 +22,13 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
         if (httpObject instanceof HttpRequest) {
+            HttpRequest httpRequest = (HttpRequest) httpObject;
+            System.out.println("请求方法名称为:" + httpRequest.method().name());
+            URI uri = new URI(httpRequest.uri());
+            if ("/favicon.ico".equals(uri.getPath())) {
+                System.out.println("请求路劲为favicon.ico");
+            }
+
             ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
