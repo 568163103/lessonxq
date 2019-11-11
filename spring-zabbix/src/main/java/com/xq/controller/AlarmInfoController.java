0ef4a7628dff3657d14031da92d21c81dca6431a
xq12311 <568163103@qq.com>
2019-11-11 14:51:09 +0800

    no message

--- spring-zabbix/src/main/java/com/xq/controller/AlarmInfoController.java
+++ spring-zabbix/src/main/java/com/xq/controller/AlarmInfoController.java
@@ -4,6 +4,8 @@ import com.xq.domain.Events;
 import com.xq.expansion.EventsExpansion;
 import com.xq.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +33,7 @@ public class AlarmInfoController {
         Map<String,Object> result = new HashMap<>(16);
         List<EventsExpansion> eventsList = eventService.findAllEvents();
         result.put("eventsList",eventsList);
+
         return result;
     }
 }
--- spring-zabbix/src/main/java/com/xq/service/impl/EventServiceImpl.java
+++ spring-zabbix/src/main/java/com/xq/service/impl/EventServiceImpl.java
@@ -8,6 +8,8 @@ import com.xq.mapper.EventsMapper;
 import com.xq.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 import java.util.List;
 
@@ -23,10 +25,13 @@ import java.util.List;
 public class EventServiceImpl  implements EventService {
     @Autowired
     private EventsExpansionMapper eventsExpansionMapper;
-
+    @Transactional(readOnly = true, rollbackFor = Exception.class)
     @Override
     public List<EventsExpansion> findAllEvents() {
         PageHelper.startPage(1,10);
+        try{}catch (Exception e){
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+        }
         return eventsExpansionMapper.findAllEvents();
     }
 }
