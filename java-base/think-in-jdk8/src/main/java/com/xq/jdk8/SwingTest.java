5048cbe1cf25c08c3bdb2b0fae9597f73be7ce06
xq123456 <568163103@qq.com>
2019-12-17 13:20:52 +0800

    no message

--- java-base/think-in-jdk8/src/main/java/com/xq/jdk8/SwingTest.java
+++ java-base/think-in-jdk8/src/main/java/com/xq/jdk8/SwingTest.java
@@ -1,5 +1,9 @@
 package com.xq.jdk8;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * @author mac-xq
  * @ClassName
@@ -8,4 +12,17 @@ package com.xq.jdk8;
  * @Version
  **/
 public class SwingTest {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("my JFrame");
+        JButton jButton = new JButton("myButton");
+        jButton.addActionListener(event -> {
+            System.out.println("Hello World");
+            System.out.println("hhhhh hahaha ");
+        });
+
+        jFrame.add(jButton);
+        jFrame.pack();
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
